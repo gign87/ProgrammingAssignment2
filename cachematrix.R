6b1be97b5ce0344ba289a6cfb4aa3a87c672256e
gign87 <matt.dobany@gmail.com>
2015-03-17 08:45:12 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions calculate the inverse of an arbitrary set matrix and cache it in the memory.
+## If the inverse has already been calculated, then it simply get the inverse from the cache, otherwise it computes it.  
 
-## Write a short comment describing this function
+## This function calculates the inverse of an input matrix. If it is a squared matrix, then the solve() function will be used,
+## otherwise the inverse will be computed by the ginv() function. The latter one can be found in the MASS package. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+           inv<-NULL
+           setmatrix<-function(y){
+                 x<<-y
+                 inv<<-NULL
+           }
+           getmatrix<-function() x
+           calculateinverse<-function() {
+                 if(ncol(x)==nrow(x)){
+                       inv<<-solve(x)
+                 } else {
+                       inv<<-ginv(x)
+                 }
+           }
+           getinverse<-function() inv
+           list(setmatrix=setmatrix,getmatrix=getmatrix,
+                calculateinverse=calculateinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## It calculates the inverese of the matrix returned by the makeCacheMAtrix function.
+## If the inverse has already been calculated, then it will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inv<-x$getinverse()
+      if(!is.null(inv)) {
+            message("getting cached inverse matrix")
+            return(inv)
+      } else {
+      inv<-x$calculateinverse()
+      inv}
 }
